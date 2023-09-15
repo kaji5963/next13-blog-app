dd2478663cd791c6aca7d56e7102b0b176cd5903
takuya_kajiwara <fd.elt.119@gmail.com>
2023-09-16 06:47:50 +0900

    ヘッダー及びフッター作成

---
+++ src/app/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <footer className='py-2 px-4 border-t'>
+      <small>@2023 kaji5963</small>
+    </footer>
+  )
+}
+
+export default footer
