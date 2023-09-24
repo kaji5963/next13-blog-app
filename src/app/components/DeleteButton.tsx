d4cee960215e6fdb5aa538b421b3c7fe210297f7
takuya_kajiwara <fd.elt.119@gmail.com>
2023-09-24 22:05:47 +0900

    削除機能実装

---
+++ src/app/components/DeleteButton.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import { deleteArticle } from '@/blogAPI';
+import { useRouter } from 'next/navigation';
+import React from 'react';
+
+const DeleteButton = ({ id }: { id: string }) => {
+  const router = useRouter();
+
+  const handleDelete = async () => {
+    await deleteArticle(id);
+
+    router.push("/");
+    router.refresh();
+  };
+  return (
+    <div
+      onClick={handleDelete}
+      className="bg-red-500 hover:bg-red-600 rounded-md py-2 px-5 inline cursor-pointer"
+    >
+      削除
+    </div>
+  );
+};
+
+export default DeleteButton;
--- src/blogAPI.ts
+++ src/blogAPI.ts
@@ -65,3 +65,19 @@ export const createArticle = async (
 
   return newArticle;
 };
+
+export const deleteArticle = async (id: string): Promise<Article> => {
+  const res = await fetch(`http://localhost:3001/posts/${id}`, {
+    method: 'DELETE',
+  });
+
+  if (!res.ok) {
+    throw new Error('error');
+  }
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const deleteArticle = await res.json();
+
+  return deleteArticle;
+};
